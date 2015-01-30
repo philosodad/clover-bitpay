20d411f3de17e21143a24ff1d03b7c7725ede6db
paul <paul@gaspode.local>
2015-01-30 11:28:48 -0500

    Initial Commit

---
+++ app/src/main/java/com/bitpay/bitpayforclover/ConfigureActivity.java
@@ -0,0 +1,76 @@
+package com.bitpay.bitpayforclover;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.bitpay.sdk.android.BitPayAndroid;
+import com.bitpay.sdk.android.interfaces.BitpayPromiseCallback;
+import com.bitpay.sdk.android.interfaces.PromiseCallback;
+import com.bitpay.sdk.controller.BitPay;
+import com.bitpay.sdk.controller.BitPayException;
+
+
+public class ConfigureActivity extends ActionBarActivity implements View.OnClickListener{
+
+    public static final String TAG = "Pairing Status: ";
+    private EditText pairingCode;
+    private Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configure);
+
+        pairingCode = (EditText) findViewById(R.id.pairingCodeField);
+        submitButton = (Button) findViewById(R.id.submit_pair);
+
+        submitButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_configure, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String clientName = "whyaname";
+        String code = pairingCode.getText().toString();
+        BitPayAndroid client = new BitPayAndroid(clientName, "https://test.bitpay.com/");
+        client.authorizeClientAsync(code).then(new PromiseCallback<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "success");
+            }
+
+            @Override
+            public void onError(BitPayException e) {
+                Log.d(TAG, "failure");
+            }
+        });
+    }
+}
